4b95cc629fd5b2b0052a103f693e929e7f2e06de
arcanlte <pinangayjoel@gmail.com>
2025-08-31 05:23:12 -0400

    Added Dockerfile

---
+++ pokemon-react/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Pokemon() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/pokemon")
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {data.map((pokemon) => (
+        <li key={pokemon.id}>
+          <img src={pokemon.frontImage} alt={pokemon.name} />
+          <img src={pokemon.backImage} alt={pokemon.name} />
+          <img src={pokemon.gifFrontImage} alt={pokemon.name} />
+          <img src={pokemon.gifBackImage} alt={pokemon.name} />
+          <h2>{pokemon.name}</h2>
+          <p>HP: {pokemon.hp}</p>
+          <p>Attack: {pokemon.attack}</p>
+          <p>Defense: {pokemon.defense}</p>
+          <p>Special-Attack: {pokemon.specialAttack}</p>
+          <p>Special-Defense: {pokemon.specialDefense}</p>
+          <p>Speed: {pokemon.speed}</p>
+        </li>
+      ))}
+    </div>
+  );
+}
